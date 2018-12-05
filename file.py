7f6a72ee6f3a0dd2e145c3d8f31ad9436b90ff6d
KarolinaIliash <karolina.iliash@gmail.com>
2018-12-06 01:56:18 +0200

    Added json serialization for states tree.

--- file.py
+++ file.py
@@ -1,6 +1,19 @@
 from lexer import Lexer
 import states
 
+import json
+
+
+class MyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, states.State):
+            d = {}
+            d['_Type'] = obj.__class__.__name__
+            d.update(obj.__dict__)
+            return d
+        if isinstance(obj, states.Scope):
+            return obj.name
+        return json.JSONEncoder.default(self, obj)
 
 class CppFile:
     def __init__(self, path):
@@ -9,10 +22,15 @@ class CppFile:
     def scan(self):
         file = open(self.path_, 'r')
         s = file.read()
-        l = Lexer(s)
-        l.parse()
+        lex = Lexer(s)
+        lex.parse()
 
-        states.cur_lexer = l
+        states.cur_lexer = lex
+        states.cur_token = 0
         nm = states.Namespace()
         nm.is_file = True
+        nm.name = self.path_
         nm.parse_me()
+
+        print(MyEncoder().encode(nm))
+
